1927bf30072f58d78998cf76b6a900f438908de8
Jordan Kostelidis <iordkost@teicm.gr>
2017-05-06 12:25:26 +0300

    Add Username and eMail Check

--- api/v1/auth/register/index.php
+++ api/v1/auth/register/index.php
@@ -3,24 +3,48 @@
 	require_once("../../../../config.php");
 	enableJSON();
 
+	function isRegisteredMail($eMail) {
+		$Query = "SELECT * FROM users WHERE email='$eMail'";
+		if(mysql_num_rows(mysql_query($Query)) == 1) {
+			return true;
+		}
+	}
+
+	function isRegisteredUsername($Username) {
+		$Query = "SELECT * FROM users WHERE username='$Username'";
+		if(mysql_num_rows(mysql_query($Query)) == 1) {
+			return true;
+		}
+	}
+
 	if(isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password'])) {
 
 		$Username = $_POST["username"];
 		$eMail = $_POST['email'];
 		$Password = md5($_POST['password']);
 
-		$Query = "INSERT INTO users(username,password, email) VALUES ('$Username','$Password','$eMail')";
+		if(isRegisteredUsername($Username)) {
+			$ObjectJSON->Status = "BAD";
+			$ObjectJSON->Token = md5("BAD");
+			$ObjectJSON->Message = "This username is used ! Use other username !";
+		} else if (isRegisteredMail($eMail)) {
+			$ObjectJSON->Status = "BAD";
+			$ObjectJSON->Token = md5("BAD");
+			$ObjectJSON->Message = "This email is used ! Use other email !";
+		} else {
+			$Query = "INSERT INTO users(username,password, email) VALUES ('$Username','$Password','$eMail')";
 
-		$Result = mysql_query($Query);
+			$Result = mysql_query($Query);
 
-		$ObjectJSON->Status = "OK";
-		$ObjectJSON->Token = md5($Username.$Password);
-		$ObjectJSON->Message = "ANTE GIA";	
+			$ObjectJSON->Status = "OK";
+			$ObjectJSON->Token = md5($Username.$Password);
+			$ObjectJSON->Message = "User registered !";
+		}
 
 	} else {
 		$ObjectJSON->Status = "BAD";
 		$ObjectJSON->Token = md5("BAD");
-		$ObjectJSON->Message = "You must give username, email and password using POST method";
+		$ObjectJSON->Message = "You must give username, email and password using POST method !";
 		
 	}
 
