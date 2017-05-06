ab53aaef5a8fec3d5c69057e52a84ea11a671942
Jordan Kostelidis <iordkost@teicm.gr>
2017-05-06 14:12:52 +0300

    update

--- api/v1/auth/register/index.php
+++ api/v1/auth/register/index.php
@@ -5,9 +5,9 @@
 
 	require_once("../auth.php");
 
-	if(isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password'])) {
+	if(isset($_POST['email']) && isset($_POST['password'])) {
 
-		$Username = $_POST["username"];
+		$Username = "debuguser";
 		$eMail = $_POST['email'];
 		$Password = md5($_POST['password']);
 
@@ -28,7 +28,7 @@
 	} else {
 		$ObjectJSON->Status = "BAD";
 		$ObjectJSON->Token = md5("BAD");
-		$ObjectJSON->Message = "You must give username, email and password using POST method !";
+		$ObjectJSON->Message = "You must give email and password using POST method !";
 		
 	}
 
